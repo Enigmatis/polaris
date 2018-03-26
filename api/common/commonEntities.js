daebe2e1b48b0c2a0b0721f5ba4f1cfe4f002bf2
Shimshon polak <yarinvak@gmail.com>
2018-03-26 15:20:22 +0300

    add common entities

--- api/common/commonEntities.js
+++ api/common/commonEntities.js
@@ -1,7 +1,7 @@
-const CommonEntity = `interface CommonEntity{
+const CommonEntityInterfaceDefinition = `interface CommonEntity{
         id: ID!
         creationDate: String,
         lastUpdateDate: String,
         dataVersion: Int!}`;
 
-module.exports = [{name: 'commonEntityInterface', types: CommonEntity}];
+module.exports = {commonEntityInterface: {types: CommonEntityInterfaceDefinition}};
