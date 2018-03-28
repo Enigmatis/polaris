bc64c31e6f207ea62a7581901b12d6c2915163f7
Shimshon polak <yarinvak@gmail.com>
2018-03-28 11:37:05 +0300

    including voyager

--- server/server.configurations.js
+++ server/server.configurations.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {graphqlExpress, graphiqlExpress} = require('apollo-server-express');
 const {makeExecutableSchema} = require('graphql-tools');
+const path = require('path');
+var cors = require('cors');
 
 // initialize the app
 function startGraphQLServer(schemaObject, port) {
@@ -9,8 +11,12 @@ function startGraphQLServer(schemaObject, port) {
     const app = express();
 
 // create endpoints
-    app.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
-    app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+    app.use('/graphql', bodyParser.json(), graphqlExpress({schema}), cors());
+    app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}), cors());
+    // // app.use('/voyager', require('../voyager'));
+    app.get('/voyager', function (req, res) {
+        res.sendFile(path.join(__dirname, '/voyager/index.html'))
+    }, cors());
 
 // run server
     app.listen(port, () => {
@@ -18,6 +24,7 @@ function startGraphQLServer(schemaObject, port) {
         console.log('----------------------------------------------------------');
         console.log('GraphQL endpoint is now running at: http://localhost:' + port + '/graphql');
         console.log('GraphiQL is now running at: http://localhost:' + port + '/graphiql');
+        console.log('Voyager is not running at: http://localhost:' + port + '/voyager');
     });
 }
 
