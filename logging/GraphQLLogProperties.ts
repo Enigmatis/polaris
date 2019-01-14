b11d8ec87e6278c40cdea3c9a124c198cf132764
chenshoo <chenshooker@gmail.com>
2019-01-14 13:52:46 +0200

    fix interface imports

--- logging/GraphQLLogProperties.ts
+++ logging/GraphQLLogProperties.ts
@@ -1,8 +1,4 @@
 import {PolarisLogProperties} from "@enigmatis/polaris-logs";
-import {EventKindDescriptionLogProperty} from "@enigmatis/polaris-logs/dist/src/entities/event-kind-description-log-property";
-import {RealityLogProperty} from "@enigmatis/polaris-logs/dist/src/entities/reality-log-property";
-import {RequestLogProperty} from "@enigmatis/polaris-logs/dist/src/entities/request-log-property";
-import {SystemLogProperty} from "@enigmatis/polaris-logs/dist/src/entities/system-log-property";
 
 export interface GraphQLLogProperties extends PolarisLogProperties {
     operationName?: string;
